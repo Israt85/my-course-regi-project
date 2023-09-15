a218a29a3d2efacf7979e69a02a2966cf35e5cc9
Israt85 <isratanjuman3@gmail.com>
2023-09-15 21:34:37 +0600

    added icons and updated something

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,7 +3,7 @@ import Cart1 from "../Cart1/Cart1";
 const Cart = ({cart,totalHr,remaining }) => {
     return (
         <div className="w-3/4 h-full bg-gray-100 rounded-2xl px-2 py-4">
-            <h3 className=" bg-slate-300 px-2 rounded-xl py-6 border-b-2 text-blue-600 font-medium text-xl">Credit Hour Remaining: {remaining} hr </h3>
+            <h3 className=" bg-slate-300 px-2 rounded-xl py-6 border-b-2 text-blue-600 font-medium text-lg">Credit Hour Remaining: {remaining} hr </h3>
             <h2 className="bg-slate-300 rounded-xl mt-4 px-2 text-2xl font-semibold border-b-2 py-6">Course name: {cart.length}</h2>
            <ol className="list-decimal list-inside">
            {
--- src/components/Course/Course.jsx
+++ src/components/Course/Course.jsx
@@ -1,3 +1,6 @@
+
+import {FaDollarSign,FaBookmark } from 'react-icons/fa'; 
+
 const Course = ({course, handleAddToSelect}) => {
     const {img, course_name, details, price, credit} =course;
     return (
@@ -6,8 +9,8 @@ const Course = ({course, handleAddToSelect}) => {
             <h3 className="text-xl font-bold pb-4">{course_name}</h3>
             <p className="text-xl font-light pb-4">{details}</p>
             <div className="flex gap-4 text-xl pb-4">
-                <p>$ Price :{price} </p>
-                <p>Credit : {credit}hr</p>
+                <p className="flex"><span className="text-2xl"><FaDollarSign/></span>Price :{price}</p>
+                <p className='flex gap-2'><span className='text-2xl'><FaBookmark/></span>Credit: {credit}hr</p>
             </div>
            <div className="w-full h-auto mt-auto text-center">
            <button onClick={()=>handleAddToSelect(course)} className=" text-center font-bold px-20 py-2 rounded-lg bg-blue-700">Select</button>
